7fac48a3d26f59f9aa54d4c407503a73bbd6bf27
KasiaWiktoria <kw_s@interia.pl>
2020-11-23 19:59:14 +0100

    bez funkcji /send

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ shipments = []
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
-
+'''
 @app.route("/send", methods=[GET])
 def send(name):
     return render_template('send.html', my_shipments = shipments)
-
+'''
 
 @app.route("/<name>", methods=["GET"])
 def set(name):
