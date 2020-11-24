f9658b65216a7b094ce2e0d8f4f5daf923f156fd
KasiaWiktoria <kw_s@interia.pl>
2020-11-24 17:54:37 +0100

    zmieniona forma import

--- static/scripts/registration.js
+++ static/scripts/registration.js
@@ -1,6 +1,6 @@
-import './form_functions.js';
-import './validation_functions.js';
-import './warning_functions.js';
+import {submitForm, updateCorrectnessMessage, prepareEventOnChange} from './form_functions.js';
+import {showWarningMessage, removeWarningMessage, prepareWarningElem, appendAfterElem} from './validation_functions.js';
+import {validateName, validateSurname, validateBDate, validatePesel, validateCountry, validatePostalCode, validateCity, validateStreet, validateHouseNr, validateLogin, validatePasswd, arePasswdsTheSame} from './warning_functions.js';
 
 document.addEventListener('DOMContentLoaded', function (event) {
 
@@ -66,5 +66,74 @@ document.addEventListener('DOMContentLoaded', function (event) {
             appendAfterElem(id, uncorrectElem);
         }
     });
+
+    
+    function prepareLoginEventOnChange(FIELD_ID, updateMessageFunction) {
+        let loginInput = document.getElementById(FIELD_ID);
+        loginInput.addEventListener("change", updateMessageFunction);
+    }
+
+    function updateLoginAvailabilityMessage() {
+        let warningElemId = "loginWarning";
+    
+        warningMessage = validateLogin();
+        if (warningMessage == "") {
+            console.log("Correct login!");
+            removeWarningMessage(warningElemId);
+            let warningMessage = "Podany login jest już zajęty.";
+    
+            isLoginAvailable().then(function (isAvailable) {
+                if (isAvailable) {
+                    console.log("Available login!");
+                    removeWarningMessage(warningElemId);
+                    AVAILABLE_LOGIN = true;
+                } else {
+                    console.log("NOT available login");
+                    showWarningMessage(warningElemId, warningMessage, LOGIN_FIELD_ID);
+                }
+            }).catch(function (error) {
+                console.error("Something went wrong while checking login.");
+                console.error(error);
+            });
+        } else {
+            console.log("Unorrect login.");
+            showWarningMessage(warningElemId, warningMessage, LOGIN_FIELD_ID);
+        }
+    }
+    
+    function updatePasswdCorrectnessMessage() {
+        let warningElemId = "passwdWarning";
+        let warningMessage = validatePasswd();
+    
+        if (warningMessage == "") {
+            removeWarningMessage(warningElemId);
+            if ((arePasswdsTheSame())) {
+                console.log("Correct password!");
+                removeWarningMessage("repeatPasswdWarning");
+            } else {
+                warningMessage = "Podany ciąg znaków nie zgadza się z hasłem podanym poniżej.";
+                showWarningMessage(warningElemId, warningMessage, PASSWD_FIELD_ID);
+            }
+        } else {
+            console.log("Uncorrect password");
+            showWarningMessage(warningElemId, warningMessage, PASSWD_FIELD_ID);
+        }
+    }
+    
+    function updateRepeatPasswdCorrectnessMessage() {
+        let warningElemId = "repeatPasswdWarning";
+        let warningMessage = "Podany ciąg znaków nie zgadza się z hasłem podanym wyżej.";
+    
+        if (arePasswdsTheSame()) {
+            console.log("Correct repeat password!");
+            removeWarningMessage(warningElemId);
+            if (validatePasswd() == "") {
+                removeWarningMessage("passwdWarning");
+            }
+        } else {
+            console.log("Uncorrect repeat password");
+            showWarningMessage(warningElemId, warningMessage, REPEAT_PASSWD_FIELD_ID);
+        }
+    }
 
 });
